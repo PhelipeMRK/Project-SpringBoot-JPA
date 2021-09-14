98345a1dc6e48a768b6a57651fc2e825fc9598a5
PhelipeMRK <phelipe.mrk0@yahoo.com.br>
2021-09-14 06:29:48 -0300

    Projeto criado

---
+++ src/main/java/com/example/demo/ProjetoWebServicesJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesJpaApplication.class, args);
+	}
+
+}
