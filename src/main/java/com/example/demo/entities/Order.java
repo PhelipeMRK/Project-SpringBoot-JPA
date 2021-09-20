2a0dc38cc5c0f77baa5b8108b67c5eac83e32aa7
PhelipeMRK <phelipe.mrk0@yahoo.com.br>
2021-09-20 16:54:02 -0300

    Entidade Pagamento e associação Um-para-um

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.demo.entities.enums.OrderStatus;
@@ -39,6 +41,10 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>(); 
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;//no caso do Um para um, estamos mapeando a entidade para ter o mesmo
+	//id. Se o pedido for cod. 5, o pagamento também será cod. 5. Nesse caso é obrigatório o cascade
 	
 	public Order() {}
 
@@ -83,6 +89,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
